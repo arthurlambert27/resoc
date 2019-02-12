1f864b548b58bd1e16d8444829dad25643a34997
Arthur Lambert <lambertarthur222@gmail.com>
2019-02-12 21:39:33 +0100

    Mise en place des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AcceuilComponent } from './composants/acceuil/acceuil.component';
 import { AuthComponent } from './auth/auth.component';
 import { ParametreComponent } from './parametre/parametre.component';
 import { PresentationComponent } from './presentation/presentation.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'accueil', component: AcceuilComponent},
+  { path: '', component: PresentationComponent},
+  { path: 'settings', component: ParametreComponent},
+  { path: 'authentification', component: AuthComponent},
+
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +25,8 @@ import { PresentationComponent } from './presentation/presentation.component';
     PresentationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
