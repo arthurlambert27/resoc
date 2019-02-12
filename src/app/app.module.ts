ae30952a9b679a27e53ea26b0706915a739dac02
Arthur Lambert <lambertarthur222@gmail.com>
2019-02-12 21:05:49 +0100

    Ajout de component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './composants/acceuil/acceuil.component';
+import { AuthComponent } from './auth/auth.component';
+import { ParametreComponent } from './parametre/parametre.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcceuilComponent
+    AcceuilComponent,
+    AuthComponent,
+    ParametreComponent,
+    PresentationComponent
   ],
   imports: [
     BrowserModule
