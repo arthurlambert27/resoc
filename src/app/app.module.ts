180ba3f2671219424497d906993e0fb9b3ebc031
arthur227 <lambertarthur222@gmail.com>
2019-02-28 18:27:16 +0100

    Modification app module
    Supression auth.compoent dans app module
    AJout de signup/signIn component
    Ajout des formulaires

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './composants/acceuil/acceuil.component';
-import { AuthComponent } from './auth/auth.component';
 import { ParametreComponent } from './parametre/parametre.component';
 import { PresentationComponent } from './presentation/presentation.component';
 import {RouterModule, Routes} from '@angular/router';
@@ -11,13 +11,18 @@ import { AuthService } from 'src/services/auth.service';
 import { CoursComponent } from './cours/cours.component';
 import { ListeCoursComponent } from './liste-cours/liste-cours.component';
 import { CoursService } from "src/services/cours.service";
+import { HttpClientModule } from "@angular/common/http";
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+
 const appRoutes: Routes = [
   { path: 'accueil', component: AcceuilComponent},
   { path: '', component: PresentationComponent},
   { path: 'settings', component: ParametreComponent},
-  { path: 'authentification', component: AuthComponent},
-  { path: "authentification", component: AuthComponent},
-  { path: "cours", component: ListeCoursComponent}
+  { path: "cours", component: ListeCoursComponent},
+  { path: "signup", component: SignupComponent}
+  
 
 ]
 
@@ -25,15 +30,18 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     AcceuilComponent,
-    AuthComponent,
     ParametreComponent,
     PresentationComponent,
     CoursComponent,
-    ListeCoursComponent
+    ListeCoursComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthService,
