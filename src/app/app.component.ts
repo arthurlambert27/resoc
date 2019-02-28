d7a962552c27c9361c7590ebb701c789c230e899
arthur227 <lambertarthur222@gmail.com>
2019-02-28 18:22:46 +0100

    Ajout de la clé firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+	construcor(){
+		const config = {
+    apiKey: "AIzaSyB-YzZiueSgvln1hLHIzW7FacQiKVnJl5Y",
+    authDomain: "resoc-1f87d.firebaseapp.com",
+    databaseURL: "https://resoc-1f87d.firebaseio.com",
+    projectId: "resoc-1f87d",
+    storageBucket: "resoc-1f87d.appspot.com",
+    messagingSenderId: "1032696498466"
+  };
+  firebase.initializeApp(config);
 
+		}
+	
 
 }
