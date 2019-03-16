fd5592c9117e670f5b3ab60572d59ecb132bd718
alex <alexandre.buisine59@gmail.com>
2019-03-16 17:39:13 +0100

    rafraichissement par timer

--- src/app/liste-cours/liste-cours.component.ts
+++ src/app/liste-cours/liste-cours.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CoursService } from 'src/services/cours.service';
+import {Observable,timer} from 'rxjs';
 
 @Component({
   selector: 'app-liste-cours',
@@ -17,7 +18,12 @@ liste_cours: any[]
   constructor(private coursService: CoursService) { }
 
   ngOnInit() {
-    this.liste_cours = this.coursService.liste_cours;
+
+   const source=timer(1000 , 5000); 
+   const subscribe=source.subscribe(
+    val=> this.liste_cours = this.coursService.liste_cours
+   );
+       
   }
 
   onSave(){
